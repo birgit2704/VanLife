2c666c577084b7cbdbfc534974ef7042a667c5df
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-06-13 17:36:04 +0200

    general page style and document title

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,5 @@
-import React from "react"
+import React from "react";
 
 export default function Footer() {
-    return (
-        <footer>&#169; 2022 #VANLIFE</footer>
-    )
+  return <footer>&#169; 2023 #VANLIFE</footer>;
 }
